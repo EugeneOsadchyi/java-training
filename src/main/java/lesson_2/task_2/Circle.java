1dca89fb6d4e6fa7a55d4203dfdb712a1ad47b19
Eugene Osadchyi <eugene.osadchyi@gmail.com>
2016-05-15 18:15:09 +0300

    Implement Lesson2 Task 2

--- src/main/java/lesson_2/task_2/Circle.java
+++ src/main/java/lesson_2/task_2/Circle.java
@@ -1,4 +1,32 @@
 package lesson_2.task_2;
 
-public class Circle {
+public class Circle extends AbstractFigure{
+    private double x;
+    private double y;
+
+    private double radius;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
 }
--- src/main/java/lesson_2/task_2/Square.java
+++ src/main/java/lesson_2/task_2/Square.java
@@ -1,4 +1,32 @@
 package lesson_2.task_2;
 
-public class Square {
+public class Square extends AbstractFigure {
+    private double x;
+    private double y;
+
+    private double width;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
 }
